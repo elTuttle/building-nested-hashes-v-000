f00a35a6c0359f8d8693dcba927962bbd7e8952a
John Tuttle <thejwt@gmail.com>
2017-09-26 20:16:40 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,15 +5,15 @@ def first_challenge
      :patriarch => {
        :name => "Lord Montague",
        :age => "53"
-   },
+     },
      :matriarch => {
        :name => "Lady Montague",
        :age => "54"
-   },
+     },
      :hero => {
        :name => "Romeo",
        :age => "15"
-   },
+     },
      :hero_friends => [
        {
           :name => "Benvolio",
